804370ee3c95062067e8ffd1a46648b80d71d0a4
awned <65061212+ceilingfans@users.noreply.github.com>
2022-03-06 17:34:06 +0800

    refactor(Token): use `logos`

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,43 +1,27 @@
-#[derive(Debug)]
-pub struct Loc {
-    pub position: u64,
-    pub line: u64,
-}
+use logos::Logos;
 
-#[derive(Debug)]
-pub enum TokenType {
-    INVALID,
+#[derive(Logos, Debug, PartialEq)]
+pub enum TokenKind {
+    #[error]
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    ERROR,
     LPAREN,
     RPAREN,
     LBRACE,
     RBRACE,
-    BANG,
-    PIPE,
     NUMBER(i64),
     IDENT(String),
     KEYWORD(String),
     UNARY(char),
 }
 
-#[derive(Debug)]
 pub struct Token {
-    pub ty: TokenType,
     pub literal: String,
-    pub location: Loc,
-}
-
-impl Default for Token {
-    fn default() -> Self {
-        Token {
-            ty: TokenType::INVALID,
-            literal: String::new(),
-            location: Loc { position: 0, line: 0 }
-        }
-    }
+    pub kind: TokenKind,
 }
 
 impl Token {
-    pub fn new() -> Self {
-        Default::default()
+    pub fn new(literal: String, kind: TokenKind) -> Self {
+        Token { literal, kind }
     }
 }
