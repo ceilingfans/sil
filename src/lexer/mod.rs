dd677d2912dc2233bc613813e7f9a6bc40f6cac9
ceilingfans <65061212+ceilingfans@users.noreply.github.com>
2022-03-07 09:22:33 +0800

    feat(Lexer): handle strings

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -8,12 +8,17 @@ pub fn lex(source: String) -> Option<Vec<Token>> {
     let mut lex = TokenKind::lexer(&source);
     let mut tokens: Vec<Token> = vec![];
     while let Some(tok) = lex.next() {
-        if tok == TokenKind::ERROR {
-            return None;
+        match tok {
+            TokenKind::Error => return None,
+            TokenKind::String => {
+                let slice = lex.slice();
+                let literal: String = slice[1..slice.len() - 1].parse().ok()?;
+                tokens.push(Token::new(literal, tok));
+            },
+            _ => {
+                tokens.push(Token::new(lex.slice().to_string(), tok));
+            }
         }
-
-        let token = Token::new(lex.slice().to_string(), tok);
-        tokens.push(token);
     }
 
     Some(tokens)
